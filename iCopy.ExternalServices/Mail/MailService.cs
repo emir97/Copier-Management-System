8e8dba1f8bebfd8f47870cb03e9062c47c61f2b5
Emir Veledar <emirveledar5@gmail.com>
2019-08-18 17:05:09 +0200

    Changes on mapper and mail service

--- iCopy.ExternalServices/Mail/MailService.cs
+++ iCopy.ExternalServices/Mail/MailService.cs
@@ -1,6 +1,12 @@
-﻿namespace iCopy.ExternalServices.Mail
+﻿using System.Threading.Tasks;
+
+namespace iCopy.ExternalServices.Mail
 {
     public class MailService : IMailService
     {
+        public Task SendMailAsync()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- iCopy.SERVICES/Mapper/Mapper.cs
+++ iCopy.SERVICES/Mapper/Mapper.cs
@@ -11,24 +11,32 @@ namespace iCopy.SERVICES.Mapper
             CreateMap<Database.Country, Model.Response.Country>().ReverseMap();
             CreateMap<Database.Country, Model.Request.Country>().ReverseMap();
             CreateMap<Model.Response.Country, Model.Request.Country>().ReverseMap();
+
             CreateMap<Database.City, Model.Response.City>().ReverseMap();
             CreateMap<Database.City, Model.Request.City>().ReverseMap();
             CreateMap<Model.Response.City, Model.Request.City>().ReverseMap();
+
             CreateMap<Database.Company, Model.Response.Company>().ReverseMap();
             CreateMap<Database.Company, Model.Request.Company>().ReverseMap();
             CreateMap<Model.Response.Company, Model.Request.Company>().ReverseMap();
+
             CreateMap<Database.Copier, Model.Response.Copier>().ReverseMap();
             CreateMap<Database.Copier, Model.Request.Copier>().ReverseMap();
             CreateMap<Model.Response.Copier, Model.Request.Copier>().ReverseMap();
+
             CreateMap<Database.Employee, Model.Response.Employee>().ReverseMap();
             CreateMap<Database.Employee, Model.Request.Employee>().ReverseMap();
-            CreateMap<Model.Response.Employee, Model.Request.Employee>().ReverseMap();         
+            CreateMap<Model.Response.Employee, Model.Request.Employee>().ReverseMap();  
+            
             CreateMap<Database.Person, Model.Request.Person>().ReverseMap();
             CreateMap<Database.Person, Model.Response.Person>().ReverseMap();
             CreateMap<Model.Request.Person, Model.Response.Person>().ReverseMap();
+
             CreateMap<Database.Client, Model.Request.Client>().ReverseMap();
+
             CreateMap<Database.ProfilePhoto, Model.Response.ProfilePhoto>().ReverseMap();
             CreateMap<Database.ProfilePhoto, Model.Request.ProfilePhoto>().ReverseMap();
+
             CreateMap<Database.ApplicationUser, Model.Request.ApplicationUserInsert>()
                 .ReverseMap()
                 .ForMember(x => x.NormalizedUserName, opt => opt.MapFrom(y => y.Username.ToUpper()))
