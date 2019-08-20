142e222ce1f9ef1333c8623c9f53861b8102ef83
Emir Veledar <emirveledar5@gmail.com>
2019-08-21 00:33:46 +0200

    Added google auth to external services

--- iCopy.ExternalServices/Mail/MailService.cs
+++ iCopy.ExternalServices/Mail/MailService.cs
@@ -1,6 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Util;
 using iCopy.ExternalServices.Model;
 using MailKit.Net.Smtp;
+using MailKit.Security;
 using MimeKit;
 using MimeKit.Text;
 
@@ -8,7 +14,7 @@ namespace iCopy.ExternalServices.Mail
 {
     public class MailService : IMailService
     {
-        public Task SendMailAsync(MailMessage message)
+        public async Task SendMailAsync(MailMessage message)
         {
             var mimeMessage = new MimeMessage();
             mimeMessage.From.Add(new MailboxAddress(message.MailServer.Name, message.MailServer.Email));
@@ -18,22 +24,16 @@ namespace iCopy.ExternalServices.Mail
             {
                 Text = message.Body
             };
-
             using (var client = new SmtpClient())
             {
                 // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
                 //client.ServerCertificateValidationCallback = (s, c, h, e) => true;
 
-                client.Connect(message.MailServer.Url, message.MailServer.Port, true);
-
-                // Note: only needed if the SMTP server requires authentication
-                client.Authenticate(message.MailServer.Username, message.MailServer.Password);
-
+                client.Connect(message.MailServer.Url, message.MailServer.Port, MailKit.Security.SecureSocketOptions.StartTls);
+                client.Authenticate(message.MailServer.Username, message.MailServer.Password, CancellationToken.None);
                 client.Send(mimeMessage);
-
                 client.Disconnect(true);
             }
-            return Task.CompletedTask;
         }
     }
 }
