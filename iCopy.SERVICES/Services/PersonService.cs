583b28046ddf9a167f8bc1c1b32c94b8a815b1ff
Kenan Količić <kenan.kolicic@edu.fit.ba>
2020-01-11 22:49:30 +0100

    profile

--- iCopy.SERVICES/Services/PersonService.cs
+++ iCopy.SERVICES/Services/PersonService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using iCopy.Database.Context;
 using iCopy.SERVICES.IServices;
+using System.Threading.Tasks;
 
 namespace iCopy.SERVICES.Services
 {
@@ -8,6 +9,25 @@ namespace iCopy.SERVICES.Services
     {
         public PersonService(DBContext ctx, IMapper mapper) : base(ctx, mapper)
         {
+        }
+        public override async Task<Model.Response.Person> UpdateAsync(int id, Model.Request.Person entity)
+        {
+            try
+            {
+                Model.Response.Person person = await base.UpdateAsync(id, entity);
+                if (entity. != null)
+                {
+                    entity.ProfilePhoto.ApplicationUserId = copier.ApplicationUserId;
+                    await ProfilePhotoService.InsertAsync(entity.ProfilePhoto);
+                }
+                // TODO: Dodati Log
+                return copier;
+            }
+            catch (Exception e)
+            {
+                // TODO: Dodati log
+                throw e;
+            }
         }
     }
 }
